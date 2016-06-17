8c5ca2d7cbeacc4cb53655adc3c2f99f37fa119b
Stephanie Bland <stephie.bland@gmail.com>
2016-06-17 15:54:44 -0300

    Updated LaTex File
    Just added more stuff, cleaned it up.  Still need to figure out how to
    reference things that happen in a figure in the main text (like getting
    basic values back - values that happen in Fig_.R.
    
    Also, I think my figure caption exceeded the proper length, I need to cut
    it down, but I also realize it messes up the format of the PDF if it's too
    long.
    
    Signed-off-by: Stephanie Bland <stephie.bland@gmail.com>

--- PAPER/Fig_lifestage_prey.R
+++ PAPER/Fig_lifestage_prey.R
@@ -1,6 +1,7 @@
 # ---- Lifestage_prey ----
 # first, your data
-lifestage=1:9
+N_stages=9
+lifestage=1:N_stages
 min_niche=c(5,7,7,8,6,11,12,10,14)
 spec_gener=c(3,5,2,4,4,1,3,6,3)
 max_niche=min_niche+spec_gener
@@ -17,6 +18,6 @@ ggplot(Prey_niche_val, aes(x=lifestage,ymin = `min_niche`,
   ylab('Prey Niche Range') +
   scale_x_continuous(breaks=lifestage)+scale_y_continuous(breaks=0:30,limits = c(0,20))
 
-
+niche_range=c(min(min_niche),max(max_niche))
 
 
