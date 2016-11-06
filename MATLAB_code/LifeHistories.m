a10f272cbde101d86615715011bd8a5d75345a45
Stephanie Bland <Bland@Dal.ca>
2016-11-06 17:14:10 -0400

    Updated Leslie Matrix
    Leslie Matrix now reflects fact that i clustered life stages by species.

--- MATLAB_code/LifeHistories.m
+++ MATLAB_code/LifeHistories.m
@@ -49,12 +49,19 @@ L_max=(W_max/q).^(1/growth_exp);%(Sangun et al. 2007)
 L_inf=(10^0.044)*(L_max.^0.9841);
 K=3./t_max;% Set according to W_inf
 t_0=t_max+((1./K).*log(1-(L_max./L_inf)));%For small adult weights (ex: W_max=88.7630), this breaks down and starts giving positive t_0
+%Temporary solution to K being too large.  I'll just force it to be small
+%enough to get a negative t_0
+for i=find(t_0>0)'
+    K(i)=-log(1-(L_max(i)/L_inf(i)))/t_max(i);
+    K(i)=0.9*K(i);%I had no justification for choosing 90%
+end
+t_0=t_max+((1./K).*log(1-(L_max./L_inf)));%Recalculate t_0 now that K is corrected.
 
 %Create a matrix lifestage_Mass/Mass_matrix that describes the weight of each life stage j
 %for each fish i (so fish species are in rows, and lifestages are in
 %columns.
-newwebsize=nichewebsize+sum(isfish.*t_max);
-Mass_matrix=zeros(nichewebsize,1+max(t_max))/0;
+newwebsize=nichewebsize+sum(isfish.*t_max);%Size of new web, including 
+Mass_matrix=zeros(nichewebsize,1+max(t_max))/0;%Just want NAN matrix of correct dimensions.
 Mass_matrix(:,1)=Mvec/W_scalar;%First column is just the weight of all species (fish rows will be overwritten with weight of youngest lifestage)
 for i=find(isfish')%only does the loop for fish species
     for t=0:t_max(i)
@@ -69,10 +76,13 @@ Mass(isnan(Mass))=[];%Alternate identical method: Mass=Mass(find(isnan(Mass)==0)
 %Get a vector that says what species each lifestage is part of
 N_stages=isfish+t_max;%Number of lifestages for each species. Necessary because fish with t_max=1 means it has 2 lifestages, and didn't want to use 0 for other species because K=3/t_max doesn't like it.
 species=[];%Vector saying what species each new node is part of.
+orig_nodes=[];%Vector that says which nodes are original nodes (so new lifestages are 0, and species that were in the original model are 1.
 for i=1:nichewebsize
     j=N_stages(i);
     species=[species, repelem(i,j)];
+    orig_nodes=[orig_nodes, repelem(0,j-1),1];
 end
+orig_species=orig_nodes.*species;%Enumerates the original species
 
 %%-------------------------------------------------------------------------
 %%  LIFE HISTORY MATRIX
@@ -86,7 +96,7 @@ end
 
 %Fish life history tables:  Creates a Leslie matrix where aij is the contribution of life stage j to life stage i.
 lifehistory_table=eye(newwebsize);%Identity Matrix for life history table, so non-fish are untransformed by matrix
-temp_index=nichewebsize;
+%temp_index=nichewebsize;%Old code for when I didn't have lifestages clustered by species.
 for i=1:nichewebsize
     stages=N_stages(i);%Number of fish life history stages
     if stages~=1
@@ -99,8 +109,9 @@ for i=1:nichewebsize
         lifehis_breed(1,:)=fert;%Set the first row to the fertility rate;
         lifehis_breed=lifehis_breed+diag(non_mature);%Set the diagonal to the probability of not maturing to the next stage, but staying the same age.
         %So now, incorporate it into life history table
-        lifehistory_table([i,temp_index+(1:stages-1)],[i,temp_index+(1:stages-1)])=lifehis_breed;
-        temp_index=temp_index+stages-1;
+        %lifehistory_table([i,temp_index+(1:stages-1)],[i,temp_index+(1:stages-1)])=lifehis_breed;%Not this one - I chose to cluster lifestages together to keep species in groups
+        lifehistory_table(i:(i+stages-1),i:(i+stages-1))=lifehis_breed;
+        %temp_index=temp_index+stages-1;
     end
 end
 
