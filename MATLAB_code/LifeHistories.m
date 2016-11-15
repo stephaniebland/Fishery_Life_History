ccf6dd695acb725c4b9b8567a46731ec78ba549a
Stephanie Bland <Bland@Dal.ca>
2016-11-15 15:43:01 -0700

    Moved Leslie Matrix to New File
    This Way I can call the Leslie Matrix Function again in webdriver.

--- MATLAB_code/LifeHistories.m
+++ MATLAB_code/LifeHistories.m
@@ -84,30 +84,7 @@ orig_index=find(orig_nodes');%index of original species
 %%-------------------------------------------------------------------------
 %%  LIFE HISTORY MATRIX - LESLIE MATRIX
 %%-------------------------------------------------------------------------
-%Suppose you have a fish with 4 life stages.  Then you can create a
-%lifehistory matrix for it.  Find correlations from Hutchings, J. A., Myers, R. A., García, V. B., Lucifora, L. O., & Kuparinen, A. (2012). Life-history correlates of extinction risk and recovery potential. Ecological Applications, 22(4), 1061–1067. Retrieved from http://www.esajournals.org/doi/abs/10.1890/11-1313.1
-%This relates age at maturity, max litter size, and weight to growth rate.
-%But since both weight and age are fixed, well, you could either adjust age
-%accordingly (you just calculated age). Are you sure you want to use this,
-%or is there something better?
-
-%Fish life history tables:  Creates a Leslie matrix where aij is the contribution of life stage j to life stage i.
-lifehistory_table=eye(newwebsize);%Identity Matrix for life history table, so non-fish are untransformed by matrix
-for i=1:nichewebsize
-    stages=N_stages(i);%Number of fish life history stages
-    if stages~=1
-        aging=1*ones(1,stages-1);%length of stages-1, some sort of distribution
-        fert=.5*ones(1,stages);%length of stages, some sort of distribution
-        non_mature=zeros(1,stages);%Default for fish that don't mature is 0, they either mature or die.
-        %NOTE!  The order of the following lines IS important!!!
-        %lifehis_breed=zeros(stages);%Reset matrix from last run.
-        lifehis_breed=diag(aging,-1);%Set the subdiagonal to the probability of maturing to the next stage
-        lifehis_breed(1,:)=fert;%Set the first row to the fertility rate;
-        lifehis_breed=lifehis_breed+diag(non_mature);%Set the diagonal to the probability of not maturing to the next stage, but staying the same age.
-        %So now, incorporate it into life history table
-        lifehistory_table(i:(i+stages-1),i:(i+stages-1))=lifehis_breed;
-    end
-end
+[lifehistory_table]= LeslieMatrix(nichewebsize,newwebsize,N_stages);
 
 %%-------------------------------------------------------------------------
 %%  NEW NICHEWEB - NEO'S METHOD - SPLIT OLD DIET
