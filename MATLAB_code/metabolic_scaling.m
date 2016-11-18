c467817788e0ae27c2badba9a8a555686a908e2c
Stephanie Bland <Bland@Dal.ca>
2016-11-18 16:37:44 -0700

    Updated fish_props.m
    fish_props now has accurate references to orig. structures
    And minor bug fix: Z was a matrix before.

--- MATLAB_code/metabolic_scaling.m
+++ MATLAB_code/metabolic_scaling.m
@@ -13,16 +13,16 @@
 %--------------------------------------------------------------------------
 
 % uncomment if to use as a function
-function [meta,Z]= metabolic_scaling(meta_scale,nichewebsize,basalsp,isfish,T,Mass,Z_old,orig_nodes)
+function [meta,Z]= metabolic_scaling(meta_scale,nichewebsize,basalsp,isfish,T,Mass,orig_Z,orig_nodes)
 attach(meta_scale);
 %--------------------------------------------------------------------------
 % Find remaining consumer-resource body size ratios (for new life stages)
 % and calculate metabolic rates
 %--------------------------------------------------------------------------
-    Z=Mass.^(1/(T-1)); %Consumer-resource body-size
+    Z=Mass'.^(1/(T-1)); %Consumer-resource body-size
     %IMPORTANT, FILL IN OLD Z, IN CASE YOU LOSE ACCURACY FOR NEW THINGS
     %(ALTERNATIVELY GET FUNCTION TO ONLY CALCULATE FOR J>NICHEWEBSIZE)
-    Z(find(orig_nodes))=Z_old;% Fill in original Z elements for higher accuracy
+    Z(find(orig_nodes))=orig_Z;% Fill in original Z elements for higher accuracy
     %Nevermind, Z is probably not that important outside of the model anyhow. No need to stress about it.
 
 %     %%Metabolic scaling constants  %%%No longer used
--- fish_props.m
+++ fish_props.m
@@ -8,29 +8,29 @@
 
 %% Vectors of length S_0 (number of species)
 % n_new,c_new,r_new don't change
-% T1_old',T2_old Trophic levels change with a new foodweb
-% Z_old should be saved because I don't know if I will change Z for new nodes yet.
-% Mvec_old Save old mass because you scale it before applying lifehistory
-l10_Mvec=log10(Mvec_old);%Calculate now because it's more accurate this way
-ln_Mvec=log(Mvec_old);
-orig_vec=[n_new,c_new,r_new,T1_old',T2_old,Z_old,Mvec_old,l10_Mvec,ln_Mvec,N_stages];
+% orig.T1',orig.T2 Trophic levels change with a new foodweb
+% orig.Z should be saved because I don't know if I will change Z for new nodes yet.
+% orig.Mvec Save old mass because you scale it before applying lifehistory
+l10_Mvec=log10(orig.Mvec);%Calculate now because it's more accurate this way
+ln_Mvec=log(orig.Mvec);
+orig_vec=[n_new,c_new,r_new,orig.T1',orig.T2,orig.Z,orig.Mvec,l10_Mvec,ln_Mvec,N_stages];
 
 %% Vectors of length nichewebsize (number of nodes)
 % B_orig I don't know how much it will change in the first time step of ODE
 % T1,T2 Changes for new nicheweb
 % meta,Z,Mass Obvious
-l10_Mass=log10(Mass');%Calculate now because it's more accurate this way
-ln_Mass=log(Mass');
-% orig_nodes',species',N_stages Probably not necessary to have all of these, but I don't care
+l10_Mass=log10(Mass);%Calculate now because it's more accurate this way
+ln_Mass=log(Mass);
+% orig.nodes',species',N_stages Probably not necessary to have all of these, but I don't care
 % int_growth Intrinsic growth rate for basal species
-new_vec=[B_orig,T1',T2,meta,Z',Mass',l10_Mass,ln_Mass,orig_nodes',species',isfish,lifestage,int_growth,harv,B0,E0];
+new_vec=[B_orig,T1',T2,meta,Z',Mass,l10_Mass,ln_Mass,orig.nodes',species',isfish,lifestage',int_growth,harv,B0,E0];
 
 %% Variables
 variables=[S_0,connectance,nan_error,R_squared,Adj_Rsq,lin_regr',r_i_mean,r_i_std,r_i_min,r_i_max,K_param,f_a,f_m,q,t_init,t_final,ext_thresh,mu,ca,co,p_a,p_b];
 
 %% Other things to store
 % nicheweb
-% nicheweb_old %because it's way too complicated to figure out original fish diet, since I don't know how I'm going to split it yet.  Might as well guarantee accuracy for something as important as this.
+% orig.nicheweb %because it's way too complicated to figure out original fish diet, since I don't know how I'm going to split it yet.  Might as well guarantee accuracy for something as important as this.
 sim_run=[year_index,full_t,full_sim];
 % adj_list
 % max_assim
@@ -42,7 +42,7 @@ sim_run=[year_index,full_t,full_sim];
 % basalsp
 % Top species (species without predators - but if you split into life histories more species will have predators!)
 % TrophLevel=(T1'+T2)/2
-% isfish_old=isfish(find(orig_nodes))
+% orig.isfish=isfish(find(orig.nodes))
 % Cannibals, Herbivores, Omnivores
 % meta_N_stages=repelem(N_stages,N_stages)
 
@@ -52,12 +52,12 @@ sim_run=[year_index,full_t,full_sim];
 
 %% Built in Properties
 % It's probably easier to just call this function at various time steps - so you can get properties every 100 years or so. 
-structproperties_orig=web_properties(nicheweb_old,T1_old,(T1_old'+T2_old)/2); %calculates the structural properties of the original foodweb
+structproperties_orig=web_properties(orig.nicheweb,orig.T1,(orig.T1'+orig.T2)/2); %calculates the structural properties of the original foodweb
 structproperties_start=web_properties(nicheweb,T1,TrophLevel); %calculates the structural properties of the foodweb at the beginning of simulation
 surv_nodes=find(B_end>ext_thresh);
 extant=@(x) x>ext_thresh;%Extinction threshold step function
 surv_sp_orig=accumarray(species',extant(B_end),[],@max);%Index of species from original food web that survive (so S_0 length)
 surv_sp=ismember(species',find(surv_sp_orig));%Index of species that survive in extended web
-structproperties_end=web_properties(nicheweb,T1(surv_sp),TrophLevel(surv_sp)); %calculates the structural properties of the niche
+structproperties_end=web_properties(nicheweb(surv_sp,surv_sp),T1(surv_sp),TrophLevel(surv_sp)); %calculates the structural properties of the niche
 
 
