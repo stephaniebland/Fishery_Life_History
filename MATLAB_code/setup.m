abc31ce9fe5fb09e4b8d66f86777fdbfb877fd2c
Stephanie Bland <Bland@Dal.ca>
2016-11-19 01:02:55 -0700

    Minor Updates
    - Biggest change: Guarantee food web has at least one fish species.

--- MATLAB_code/setup.m
+++ MATLAB_code/setup.m
@@ -9,26 +9,25 @@
 %--------------------------------------------------------------------------
 
 
+orig.isfish=0;
+while sum(orig.isfish)==0%Guarantee that the food web has at least one fish
 %%-------------------------------------------------------------------------
 %%  NICHE MODEL
 %%-------------------------------------------------------------------------
-
-%to construct niche web, uncomment the following lines.
     [orig.nicheweb,n_new,c_new,r_new] = NicheModel(S_0, connectance);%Create a connected (no infinite degrees of separation) foodweb with realistic species (eg. no predators without prey), and no isolated species.
-
-%or enter custom web (rows eats column)
-    %nicheweb = [0 0 0; 1 0 0; 0 1 0];
-
     nichewebsize = length(orig.nicheweb);%Steph: Find number of species (not sure why, already have S_0)
     basalsp = find(sum(orig.nicheweb,2)==0);%List the autotrophs (So whatever doesn't have prey)  Hidden assumption - can't assign negative prey values (but why would you?)
 
 %%-------------------------------------------------------------------------
-%%  FIRST: SET DYNAMICS PARAMETERS
+%%  SET DYNAMICS PARAMETERS
 %%-------------------------------------------------------------------------
 %Calculates species weight -> so you know how many life stages it needs
-%"meta", "TrophLevel" & "T1", "IsFish" and "Z"
     [TrophLevel,orig.T1,orig.T2]= TrophicLevels(nichewebsize,orig.nicheweb,basalsp);
     [orig.Z,orig.Mvec,orig.isfish]= MassCalc(masscalc,nichewebsize,basalsp,TrophLevel);
+end    
+%%-------------------------------------------------------------------------
+%%  LINEAR REGRESSION
+%%------------------------------------------------------------------------- 
     % Use Linear regression to estimate slope of mass-niche relationship:
     [R_squared,Adj_Rsq,lin_regr]=Linear_Regression(orig,n_new);
 
@@ -107,7 +106,8 @@
 %3) non linear and non isoelastic inverse demand curve
     %p_a=1000;
     %p_b=10; % set to zero to fix the price
- 
+
+%DOESNT EVEN MAKE SENSE TO DO THIS BEFORE YOU KNOW WHICH SPECIES SURVIVE!!!
 %assign harvesting link to a top species
 %---------------------------------------
 %1) randomly among top or nonbasal species
@@ -139,12 +139,13 @@
 
 %initial Biomass
 %---------------
-%1) randomly set between 0.02 and 20, from a uniform distb:
-     B0 = (999*rand(nichewebsize,1)+1)*.01;
-     %B0(find(is_split))=B0(find(is_split))/60;% Maybe try to tweak original fish densities
-     if lstages_B0ratedR==true
-         B0=B0.*orig.nodes';%Start with adults only.
-     end
+%1) Random uniform distribution in interval (0.01,10)
+    B0 = (999*rand(nichewebsize,1)+1)*.01;
+    if lstages_B0ratedR==true
+        B0=B0.*orig.nodes';%Start with adults only.
+    elseif lstages_B0ratedR~=false
+        clear B0;%Make sure there's an error if you misspell setting
+    end
 %2) from uniform distribution in the ranges 5-500, 2-200 and 1-100 
     %B0 = (99*rand(nichewebsize,1)+1).*[5; 2; 1];
 %3) set manually, example on 2 species
