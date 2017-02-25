552514c9ca55f9d467ae1b7cde86fbd41db4dc4a
Stephanie Bland <Bland@Dal.ca>
2017-02-25 10:08:52 -0400

    Got rid of unnecessary variable names
    web_mx isn’t required; nicheweb will suffice.

--- MATLAB_code/NicheModel.m
+++ MATLAB_code/NicheModel.m
@@ -32,19 +32,17 @@ function [orig]= NicheModel(cannibal_invert,num_species, connectance)
         ok_n=1; % ok_n=1 when it's a suitable web. Assume true until you test it
         n = rand(num_species,1);% assigns niche values from a uniform distribution
 
-        [web_mx,orig.n_new,orig.r_new,orig.c_new]=CreateWeb(num_species,connectance,n);
+        [nicheweb,orig.n_new,orig.r_new,orig.c_new]=CreateWeb(num_species,connectance,n);
 
         %% Invertebrate Cannibalism
         if cannibal_invert==false
-            web_mx=web_mx-diag(diag(web_mx));
+            nicheweb=nicheweb-diag(diag(nicheweb));
         end
 
         %%-----------------------------------------------------------------
         % DISCARD UNSUITABLE WEBS
         %%-----------------------------------------------------------------
         %% 1. Discard Webs with isolated species
-        nicheweb=web_mx;
-
         zerocols = find(sum(nicheweb,1)==0); %columns with only zero (they are not eaten by anyone)
         basalsp = find(sum(nicheweb,2)==0); %they don't eat anyone
         qq = intersect(basalsp,zerocols); %both: index of isolated species
@@ -61,7 +59,7 @@ function [orig]= NicheModel(cannibal_invert,num_species, connectance)
         end
 
         %% 3. Discard webs that don't have a close enough connectance value
-        links = sum(web_mx>0);  %% indices of links (which element in web_mx?)
+        links = sum(nicheweb>0);  %% indices of links (which element in nicheweb?)
         C_web = sum(links)/(num_species^2);  %% Actual connectance
         if abs(C_web-connectance)*1.0/connectance>errorconnec
             ok_n=0;
@@ -88,11 +86,11 @@ function [orig]= NicheModel(cannibal_invert,num_species, connectance)
         
     end
 
-    orig.nicheweb=nicheweb;
-    %--------------------------------------------------------------------------
-    % stop searching for a possible web after too many tries
+    %% stop searching for a possible web after too many tries
     if tries==0
         error('Impossible to create a foodweb within the number of tries you set')
     end
+    
+    orig.nicheweb=nicheweb;
 end
 
