615ea97e2ae5c5457a38003f75e9277ca45f7dba
Stephanie Bland <Bland@Dal.ca>
2017-04-05 16:55:08 -0700

    FIXED - my assumption of it being stable was wrong

--- MATLAB_code/MassCalc.m
+++ MATLAB_code/MassCalc.m
@@ -59,13 +59,12 @@ attach(masscalc);
     A=nicheweb.*Z;%Setup weighted nicheweb matrix - this is like the standard matrix used for Dijkstra algorithm, except weights represent allometric scaling instead of edge length (so multiplicative instead of additive)
     
     % Assign mass for non basal species
-    k=0; %YES, *of course* you can use same method with Z=[1 1 ...1] for calculating Trophic levels T1, and it's prob cleaner, but both methods work. 
-    while k<2%We need to iterate the while loop one extra time than min req'd to calculate all Masses - because you only just got the right mass for all the prey species of an apex predator
+    %YES, *of course* you can use same method with Z=[1 1 ...1] for calculating Trophic levels T1, and it's prob cleaner, but both methods work. 
+    for k=0:nichewebsize%We need to iterate the while loop one extra time than min req'd to calculate all Masses - because you only just got the right mass for all the prey species of an apex predator
         C=A*diag(Mass1);%Find shortest paths - so C_ij is the mass if it were calculated using path going from pred i to prey j.
         C(C==0)=NaN;%Don't mistake 0s for shortest path.  
         Mass1=min(C,[],2);%Find smallest path for each predator now that you excluded 0s
         Mass1(basalsp)=Z(basalsp);%Redefine basal species mass
-        k=k+1-isnan(sum(Mass1));%We need to do one additional loop after finding all the Masses, because the ones that we found last need to update each other.
     end
     Mass1
     
