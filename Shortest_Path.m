489077ea8b59638e13569ae336b940fa50336f8b
Stephanie Bland <Bland@Dal.ca>
2017-04-06 07:34:13 -0700

    Works for huge numbers

--- Shortest_Path.m
+++ Shortest_Path.m
@@ -7,16 +7,17 @@ nicheweb=   [0 0 0 0 0 0 0 0 0; %1
              0 0 0 0 1 0 1 0 0; %6
              0 1 0 0 0 0 0 0 0; %7
              0 0 0 0 0 1 0 0 0; %8
-             0 0 0 0 0 0 0 1 0];%9                 
-         
+             0 0 0 0 0 0 0 1 0];%9
+
 nichewebsize = length(nicheweb);
 basalsp = find(sum(nicheweb,2)==0);
 
-    edge_weight=randi(10,nichewebsize,1)-5
-    Z=exp(edge_weight)
+    maxim=10000;%Matlab doesn't like really large numbers, so if the values are huge we should reduce them first. I imagine the same holds for extremely small values.
+    edge_weight=randi(maxim,nichewebsize,1)-5;
+    Z=exp(edge_weight*100/maxim);
     
-    Mass1=zeros(nichewebsize,1);  % Set up vector
-    Mass_old=Mass1
+    Mass1=zeros(nichewebsize,1);% Set up vector
+    Mass_old=Mass1;
     Mass1(1)=1; % Basal species defined to have mass equivalent to Z
     
     A=nicheweb.*Z;%Setup weighted nicheweb matrix - this is like the standard matrix used for Dijkstra algorithm, except weights represent allometric scaling instead of edge length (so multiplicative instead of additive)
@@ -24,11 +25,11 @@ basalsp = find(sum(nicheweb,2)==0);
     
     
     while sum(Mass_old~=Mass1)~=0 %We need to iterate the while loop extra times than min req'd to calculate all Masses - because you only just got the right mass for all the prey species of an apex predator. Since Z is positive, it won't change after the longest possible simple path.
-        Mass_old=Mass1
-        C=A*diag(Mass1)%Find shortest paths - so C_ij is the mass if it were calculated using path going from pred i to prey j.       
-        C(C==0)=NaN;%Don't mistake 0s for shortest path.  
+        Mass_old=Mass1;
+        C=A*diag(Mass1);%Find shortest paths - so C_ij is the mass if it were calculated using path going from pred i to prey j.
+        C(C==0)=NaN;%Don't mistake 0s for shortest path.
         Mass1=min(C,[],2);%Find smallest path for each predator now that you excluded 0s
-        Mass1(basalsp)=Z(basalsp);%Redefine basal species mass     
+        Mass1(basalsp)=Z(basalsp);%Redefine basal species mass
     end
-    [log(Mass1), edge_weight]
+    [log(Mass1)*maxim/100, edge_weight]
     [Mass1, Z];
