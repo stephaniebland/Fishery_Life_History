ab5761c33cfb1ae7dc2821ee7b9bc3ff804866c8
Stephanie Bland <Bland@Dal.ca>
2017-04-07 09:39:08 -0700

    Version I'm keeping

--- MATLAB_code/MassCalc.m
+++ MATLAB_code/MassCalc.m
@@ -53,26 +53,20 @@ attach(masscalc);
 %Set body size based on trophic level
 %--------------------------------------------------------------------------
     %% Calculate Mass according to T1 (Shortest Distance)
-    Z_0=Z;
-    origstuff=randi(10,nichewebsize,1)-5
-    Z=exp(origstuff)
-    
-    Mass1=NaN(nichewebsize,1);  % Set up vector
+    Mass1=NaN(nichewebsize,1); % Set up vector
     Mass1(basalsp)=Z(basalsp); % Basal species defined to have mass equivalent to Z
-    
+
     A=nicheweb.*Z;%Setup weighted nicheweb matrix - this is like the standard matrix used for Dijkstra algorithm, except weights represent allometric scaling instead of edge length (so multiplicative instead of additive)
-    A(6,7)=exp(15)
     
     % Assign mass for non basal species
-    % YES, *of course* you can use same method with Z=[1 1 ...1] for calculating Trophic levels T1, and it's prob cleaner, but both methods work. 
-    for k=0:nichewebsize%We need to iterate the while loop extra times than min req'd to calculate all Masses - because you only just got the right mass for all the prey species of an apex predator. Since Z is positive, it won't change after the longest possible simple path.
+    %YES, *of course* you can use same method with Z=[1 1 ...1] for calculating Trophic levels T1, and it's prob cleaner, but both methods work. 
+    for k=0:nichewebsize%We need to iterate the while loop one extra time than min req'd to calculate all Masses - because you only just got the right mass for all the prey species of an apex predator
         C=A*diag(Mass1);%Find shortest paths - so C_ij is the mass if it were calculated using path going from pred i to prey j.
         C(C==0)=NaN;%Don't mistake 0s for shortest path.  
         Mass1=min(C,[],2);%Find smallest path for each predator now that you excluded 0s
         Mass1(basalsp)=Z(basalsp);%Redefine basal species mass
     end
-    [log(Mass1), origstuff]
-    
+    Mass1
     
     %% Calculate Mass according to T2 (Prey-Averaged Trophic Position)
     prey=sum(nicheweb,2); %sum of each row
