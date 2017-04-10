a8aea004e4f3060e50dbba3ae2cbce6fe1e746c0
Stephanie Bland <Bland@Dal.ca>
2017-04-10 12:11:07 -0700

    Bash Script Initial

---
+++ cluster.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# The looping script to run on ACENET clusters.
+###############################################
+# ACENET (www.ace-net.ca) provides cluster computing resources for researchers at Dal (and elsewhere).
+# Your thesis supervisor will have to get an account before you can, but both are free.
+# The process is described in more detail at https://www.ace-net.ca/wiki/Get_an_Account.
+#
+# If you have never used a cluster before you will want some training.
+# There are several classroom and web training sessions scheduled for early May;
+# see http://www.ace-net.ca/training/workshops-seminars/ for details.
+###############################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
