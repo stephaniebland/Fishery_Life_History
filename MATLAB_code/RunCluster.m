2426f4fe43fc2688b78033c6a34aa81d3f0293be
Stephanie Bland <Bland@Dal.ca>
2017-04-12 15:24:01 -0700

    Didn't test yet

--- MATLAB_code/RunCluster.m
+++ MATLAB_code/RunCluster.m
@@ -2,9 +2,36 @@ function [answer]= RunCluster(seed_0,simnum,lifestages_linked,Adults_only)
     %% Set Seed
     rng(seed_0+simnum)
     
-    %% Simulation
-    answer=seed_0+simnum*lifestages_linked^Adults_only
+    %% Initial Setup
+    beep off
+    warning off MATLAB:divideByZero;
+    global reprod cont_reprod Effort;
     
+    %% Protocol Parameters
+    Parameters;
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% Simulation
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %% 1st Simulation: Extended_nicheweb + Lifehistory: B_orig & linked
+    lifestages_linked=true;
+    B0=B_orig;
+    simulations;
+    save(strcat('complete_',num2str(simnum)))
+
+    %% 2nd Simulation: Extended_nicheweb: B_orig & NOT linked
+    lifestages_linked=false;
+    B0=B_orig;
+    simulations;
+    save(strcat('extended_unlinked_',num2str(simnum)))
+
+    %% 3rd Simulation: Nicheweb: B_orig=B_orig.*orig.nodes';%(Start with adults only) & NOT linked
+    lifestages_linked=false;
+    B_orig=B_orig.*orig.nodes';%Start with adults only. %CAUTION - changes total biomass, consider normalizing so all experiments have same total biomass. Or maybe sum juvenile stages to adult instead.
+    simulations;
+    save(strcat('origweb_',num2str(simnum)))
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     %% Export Data
     name=sprintf('BLANDsim=%04d-phase%d-year=%03d.txt',simnum,phase,rep)
     %dlmwrite(name,answer,',') 
