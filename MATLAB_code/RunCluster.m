f2214fb2b25e884fa410c5d1e025f398a924dad5
Stephanie Bland <Bland@Dal.ca>
2017-04-12 15:05:10 -0700

    dlmwrite save data

--- MATLAB_code/RunCluster.m
+++ MATLAB_code/RunCluster.m
@@ -1,6 +1,12 @@
 function [answer]= RunCluster(seed_0,simnum,lifestages_linked,Adults_only)
+    %% Set Seed
     rng(seed_0+simnum)
+    
+    %% Simulation
     answer=seed_0+simnum*lifestages_linked^Adults_only
-    dlmwrite('testing.txt',answer,',') 
+    
+    %% Export Data
+    name=sprintf('BLANDsim=%04d-phase%d-year=%03d.txt',simnum,phase,rep)
+    %dlmwrite(name,answer,',') 
     
 end
