e928ac6d919db9f45b2874e7b33fbad5596d497d
Stephanie Bland <Bland@Dal.ca>
2017-04-12 13:51:31 -0700

    Set Seed

--- MATLAB_code/RunCluster.m
+++ MATLAB_code/RunCluster.m
@@ -1,6 +1,6 @@
 function [answer]= RunCluster(seed_0,simnum,lifestages_linked,Adults_only)
-
+    rng(seed_0+simnum)
     answer=seed_0+simnum*lifestages_linked^Adults_only
     dlmwrite('testing.txt',answer,',') 
-
+    
 end
