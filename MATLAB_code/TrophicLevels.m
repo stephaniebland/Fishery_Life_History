1765efff6a73864e50978bbde6870833e161057c
Stephanie Bland <Bland@Dal.ca>
2017-04-13 08:02:22 -0700

    Trophic Position - Comments & Clean Up

--- MATLAB_code/TrophicLevels.m
+++ MATLAB_code/TrophicLevels.m
@@ -13,24 +13,14 @@
 %--------------------------------------------------------------------------
 
 function [T, T1,T2]= TrophicLevels(nichewebsize,nicheweb,basalsp)
-
-
-%--------------------------------------------------------------------------
-%Convert nicheweb to a numeric array and eliminate self-loops. (Eliminating
-%Cannibalism within life stages, so cannibalism is allowed between
-%different life stages).
-%--------------------------------------------------------------------------
-    nicheweb1=+nicheweb;  %Possibly a vestigial line from C++
-    %nicheweb1=nicheweb1-diag(diag(nicheweb1));%Set the diagonal to 0, so no cannibalism in Trophic calculations (but whether there is cannibalism in the model is a different question)
-
-
-%--------------------------------------------------------------------------
-%Compute shortest path to basal species for each species.
-%Note: the matrix 'nicheweb' is oriented rows eat columns. 
-%--------------------------------------------------------------------------
+    %% Shortest Path Trophic Position (T1)
+    % Compute shortest path to basal species for each species.
+    % Note: the matrix 'nicheweb' is oriented rows eat columns. 
+    
+    % Set up vector for storing values
     T1=NaN(1,nichewebsize);
 
-    %Compute shortest trophic level.
+    % Find all species with T1=1 -> Basal species (autotrophs)
     T1(basalsp)=1;  % Assign level 1 to basal species.
     ind=basalsp;    % Set up index to basal species
 
@@ -45,26 +35,24 @@ function [T, T1,T2]= TrophicLevels(nichewebsize,nicheweb,basalsp)
         end 
     end
 
-%--------------------------------------------------------------------------
-%Compute path-based trophic levels. (Levine 1980)
-%--------------------------------------------------------------------------
-
-    %Add up how many prey items each species has.
+    %% Prey Averaged Trophic Position (T2)
+    % Compute path-based trophic levels. (Levine 1980) 
+    % C=I+Q+Q^2+Q^3+.... is a geometric series & converges -> C=(I-Q)^(-1)
+    
+    % Add up how many prey items each species has:
     prey=sum(nicheweb1,2); %sum of each row
 
-    %Create unweighted Q matrix. So a matrix that gives proportion of the
-    %diet given by each prey species.
+    % Create unweighted Q matrix. So a matrix that gives proportion of the
+    % diet given by each prey species.
     Q=nicheweb1./prey;  % Create unweighted Q matrix. (Proportion of predator diet that each species gives).
     Q(isnan(Q))=0;      % Set NaN values to 0. 
     
     %Calculate trophic levels as T2=(I-Q)^-1 * 1  %Levine 1980 geometric series 
-    T2=(inv(eye(nichewebsize)-Q))*ones(nichewebsize,1);%"ones(nichewebsize,1)" or could just sum over the rows like you did everywhere else "sum(A,2)"
-    
-%--------------------------------------------------------------------------
-%Average T1 and T2  %And how is using the average even a good idea?  Why
-%not just choose favourite instead saying "both are equally valid, so we'll
-%take the most moderate approach."
-%--------------------------------------------------------------------------
-    T=((T1+T2')/2)';%So after looking into this a bit, I think that T2 is the way to go.  It's more consistent with the defn' for trophic level used by fishbase.org It might be useful to calculate the trophic levels again at the end once you know equilibrium densities. 
+    T2=(inv(eye(nichewebsize)-Q))*ones(nichewebsize,1); % Or sum over the rows "sum(A,2)"
 
-    end
+    %% Short weighted Trophic position
+    % Better estimate of Trophic position than T1 or T2 on their own:
+    % Carscallen et al. Estimating trophic position in marine and estuarine food webs (2012)
+    T=((T1+T2')/2)';
+
+end
