ca9ad72a6ce781dd51f1aceb7645f2751dd4c84e
Stephanie Bland <Bland@Dal.ca>
2017-04-15 19:58:07 -0400

    Import vars testing stuff

--- MATLAB_code/RunCluster.m
+++ MATLAB_code/RunCluster.m
@@ -50,7 +50,7 @@ function RunCluster(seed_0,simnum,Exper)
     end
     
     %% Name for Exporting Data
-    name=sprintf('BLANDseed%04d_sim%06d_link%d_AdultOnly%d_Exper%d',seed_0,simnum,lifestages_linked,Adults_only,Exper)
+    name=sprintf('BLANDseed%d_sim%d_link%d_AdultOnly%d_Exper%d',seed_0,simnum,lifestages_linked,Adults_only,Exper)
     
     %% Time Series Simulation (& Export TS dependent Properties)
     simulations;
@@ -66,10 +66,74 @@ function RunCluster(seed_0,simnum,Exper)
     dlmwrite(strcat(name,'_num_years.txt'),cell2mat(struct2cell(num_years)));
     dlmwrite(strcat(name,'_export_vals.txt'),export_vals);
     
+    import_vars={'B','B_year_end','B_stable_phase','export_int','export_real','export_vals'}
+    
+    vname=@(x) inputname(1);
+    toto=pi
+    s=vname(toto)
+
+    s=vname(import_vars)
+    test=import_vars(2)
+    vname(test)
+    import_vars(2)
+    
+    vname(import_vars)
+    vname(import_vars(2))
+    string(import_vars(2))
+    vname(string(import_vars(2)))
+    text2expr(import_vars)
+    
+  
+    
+    for item=import_vars
+        dlmwrite(strcat(name,'_',char(item),'.txt'),eval(char(item)));
+    end
+    
+    for i=1:6
+        dlmwrite(strcat(name,'_',char(import_vars(i)),'.txt'),eval(char(import_vars(i))));
+    end
+    
+    
+    dlmwrite(strcat(name,'_',import_vars(2),'.txt'),eval(char(import_vars(2))));
+    
+    
+    
+    
+    
+    
+    
+    strncmp ('hi',3)
+    
+    
+    blah=who
+    
+    a=load('hi.mat')
+    
+names = fieldnames(a)
+var_idx = strmatch('B', blah)
+HW_A = getfield(a, names{4})
+
+
+xk=strmatch(string(import_vars(1)),who)
+getfield(a, names{xk})
+trythis=genvarname(import_vars)
+trythis(1)
+    
 end
 
+eval('B')
+eval(char(import_vars(2)))
+
+char(import_vars(1))
 
 
+a = load('hi.mat')
+HW_A = a.HW_A_156
+names = fieldnames(a)
+HW_A = getfield(a, names{1})
 
+names = fieldnames(a)
+var_idx = strmatch('b_size', names)
+HW_A = getfield(a, names{var_idx})
 
 
