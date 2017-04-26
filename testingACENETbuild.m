1c325440cc20a01e11abe414cf4633006825b54a
Stephanie Bland <Bland@Dal.ca>
2017-04-26 17:33:24 -0300

    acenet testing files ehhhhhh no need to save any of this

---
+++ testingACENETbuild.m
@@ -0,0 +1,15 @@
+deploytool -build TESTcompile
+deploytool -build RunClusterTEST
+
+seedtest=0;
+% for simnum=1:10
+%     for Exper=1:3
+%         RunCluster(seed_0,simnum,Exper);
+%     end
+% end
+
+
+
+
+
+
---
+++ testingACENETpack.m
@@ -0,0 +1,15 @@
+deploytool -package TESTcompile2
+deploytool -package RunClusterTEST2
+
+seedtest=0;
+% for simnum=1:10
+%     for Exper=1:3
+%         RunCluster(seed_0,simnum,Exper);
+%     end
+% end
+
+
+
+
+
+
