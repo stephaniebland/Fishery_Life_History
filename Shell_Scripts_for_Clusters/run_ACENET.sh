bc4ac095ce45f8bbc880c450437288c63554a626
Stephanie Bland <Bland@Dal.ca>
2017-05-16 12:59:14 -0300

    Merge branch 'master' into ACENET-RUNS
    # Conflicts:
    #	run_ACENET.sh

--- Shell_Scripts_for_Clusters/run_ACENET.sh
+++ Shell_Scripts_for_Clusters/run_ACENET.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Variable Names:
 script_name=RunCluster 	# Name of the file we will be compressing
-version=.2				# Version
+version=_3				# Version
 #cluster_name=fundy		&& URL=titanium@fundy.ace-net.ca		&& dtnURL=$URL
 cluster_name=glooscap	&& URL=titanium@glooscap.ace-net.ca		&& dtnURL=titanium@dtn.glooscap.ace-net.ca
 #cluster_name=placentia	&& URL=titanium@placentia.ace-net.ca	&& dtnURL=$URL
@@ -19,32 +19,26 @@ DATE=`date +%Y%b%d`
 run_name=$DATE$version # Name of the Run, where we store the ACENET file
 
 # On my Mac Run:
-git push origin master
-git push origin ACENET-RUNS
+git push origin master ACENET-RUNS
 git bundle create ~/Documents/master\'s\ Backup/backup_$DATE.bundle master ACENET-RUNS
 # git bundle create ~/Documents/master\'s\ Backup/backup_$DATE_all.bundle --all #Stores all branches
 
 # On Selenium Run:
 ssh $myLinux <<END
-	# Run:
 	rm -rf masters/
 	git clone -b ACENET-RUNS ~/GIT/masters.git/
 	/usr/local/MATLAB/R2017a/bin/matlab -nodisplay -r "cd('~/masters/');mcc -m $script_name.m;quit"
-END
-ssh $myLinux <<END
 	cd ~/masters
-	# On ACENET Run: 
 	sftp -i ~/.ssh/id_rsa$cluster_name $dtnURL <<END
-		mkdir /home/titanium/$run_name
+		mkdir ~/$run_name
 		cd $run_name
 		put $script_name
 		put run_$script_name.sh
-		put BLANDparams.sh
 	END
 END
 
 # Create Job Scripts Locally
-for simnum in `seq 0 19`; do
+for simnum in `seq 0 4`; do
 	declare -i simnum_0=$simsize*$simnum+1
 	declare -i simnum_f=$simsize+$simnum_0-1
 	job_name=run_simnum_0_to_$simnum_f.job
@@ -53,8 +47,7 @@ cat > $job_name<<EOF
 #$ -cwd
 #$ -j yes
 #$ -l h_rt=48:0:0
-#$ -l h_vmem=8G
-module load matlab-runtime/r2017a
+#$ -l h_vmem=10G
 ./run_$script_name.sh $MCR $seed_0 $simnum_0 $simnum_f
 EOF
 
