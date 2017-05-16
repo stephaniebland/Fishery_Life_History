0a3a9c94c7a354bb73d124b7eb0ae88015248fc7
Stephanie Bland <Bland@Dal.ca>
2017-05-16 09:32:10 -0300

    Try New ACENET setup

--- Shell_Scripts_for_Clusters/run_ACENET.sh
+++ Shell_Scripts_for_Clusters/run_ACENET.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 # Variable Names:
-script_name=RunCluster # Name of the file we will be compressing
-run_name=may15.8 # Name of the Run, where we store the ACENET file
-#cluster_name=fundy		&&	URL=titanium@fundy.ace-net.ca
-cluster_name=glooscap	&&	URL=titanium@dtn.glooscap.ace-net.ca
-#cluster_name=placentia	&&	URL=titanium@placentia.ace-net.ca
+script_name=RunCluster 	# Name of the file we will be compressing
+version=.0				# Version of the
+#cluster_name=fundy		&& URL=titanium@fundy.ace-net.ca
+cluster_name=glooscap	&& URL=titanium@dtn.glooscap.ace-net.ca
+#cluster_name=placentia	&& URL=titanium@placentia.ace-net.ca
+###############################################
+declare -i seed_0=0
+simsize=5
+# Options to run it locally instead
+# MCR=/Applications/MATLAB/MATLAB_Runtime/v91 # Run on my Mac
+# MCR=/usr/local/MATLAB/MATLAB_Runtime/v92 # Run on linux (Selenium)
+MCR=/usr/local/matlab-runtime/r2017a/v92 # Run on ACENET
+
+# Setup
+myLinux=selenium@129.173.34.107
+DATE=`date +%Y%b%d`
+run_name=$DATE$version # Name of the Run, where we store the ACENET file
 
 # On my Mac Run:
 git push origin master
 git push origin ACENET-RUNS
+git bundle create ~/Documents/master\'s\ Backup/backup_$DATE.bundle master ACENET-RUNS
+# git bundle create ~/Documents/master\'s\ Backup/backup_$DATE_all.bundle --all #Stores all branches
 
 # On Selenium Run:
-ssh selenium@129.173.34.107 <<END
-	#Run:
+ssh $myLinux <<END
+	# Run:
 	rm -rf masters/
 	git clone -b ACENET-RUNS ~/GIT/masters.git/
-	/usr/local/MATLAB/R2017a/bin/matlab -nodisplay -r "cd('/home/selenium/masters/');mcc -m $script_name.m;quit"
+	/usr/local/MATLAB/R2017a/bin/matlab -nodisplay -r "cd('~/masters/');mcc -m $script_name.m;quit"
 END
-ssh selenium@129.173.34.107 <<END
+ssh $myLinux <<END
 	cd ~/masters
 	# On ACENET Run: 
 	sftp -i ~/.ssh/id_rsa$cluster_name $URL <<END
@@ -29,14 +43,33 @@ ssh selenium@129.173.34.107 <<END
 	END
 END
 
-# And finally Run ACENET
-ssh -i /Users/JurassicPark/.ssh/id_rsa$cluster_name $URL <<END
-	cd $run_name
-	chmod +x ./BLANDparams.sh
-	./BLANDparams.sh > BLANDparams.job 
-	qsub BLANDparams.job 
-END
+# Create Job Scripts Locally
+for simnum in `seq 0 4`; do
+	declare -i simnum_0=$simsize*$simnum+1
+	declare -i simnum_f=$simsize+$simnum_0-1
+	job_name=run_simnum_0_to_$simnum_f.job
+	touch $job_name
+cat > $job_name<<EOF
+#$ -cwd
+#$ -j yes
+#$ -l h_rt=48:0:0
+#$ -l h_vmem=10G
+module load matlab-runtime/r2017a
+./run_$script_name.sh $MCR $seed_0 $simnum_0 $simnum_f
+EOF
 
+# And finally Run ACENET
+sftp -i ~/.ssh/id_rsa$cluster_name $URL <<END
+	cd $run_name
+	put $job_name
+END
+rm $job_name
+ssh -i ~/.ssh/id_rsa$cluster_name $URL <<END
+	cd $run_name
+	chmod +x $script_name run_$script_name.sh
+	qsub $job_name
+END
+done
 
 
 ## Manual setup
