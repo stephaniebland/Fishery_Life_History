4a9923861fe351a2dc36df01df2f83f76c76170b
Stephanie Bland <Bland@Dal.ca>
2017-05-25 20:15:55 -0300

    Error
    scheduling info:            (Collecting of scheduler job information is
    turned off)

---
+++ ERROR_live_qstat.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+# The looping script to run on ACENET clusters.
+# https://www.ace-net.ca/wiki/MATLAB_Runtime
+###############################################
+# ACENET (www.ace-net.ca) provides cluster computing resources for researchers at Dal (and elsewhere).
+# Your thesis supervisor will have to get an account before you can, but both are free.
+# The process is described in more detail at https://www.ace-net.ca/wiki/Get_an_Account.
+#
+# If you have never used a cluster before you will want some training.
+# There are several classroom and web training sessions scheduled for early May;
+# see http://www.ace-net.ca/training/workshops-seminars/ for details.
+###############################################
+# USEFUL ACENET COMMANDS:
+# Check the queue of jobs:
+# qstat 
+# qsum
+# showq
+# Check memory requirements:
+# qacct -j 6793085 | grep maxvmem
+###############################################
+# Variable Names:
+version=9 # Version
+declare -i seed_0=0
+simsize=5
+sims_per_cluster=100
+
+###############################################
+# Setup
+script_name=RunCluster # Name of the file we will be compressing
+myLinux=selenium@129.173.34.107
+declare -a avail_clusters=("fundy" "glooscap" "placentia")
+DATE=`date +%Y%b%d`
+JobID=`date +%m%d`$version
+run_name=$DATE\_$version # Name of the Run, where we store the ACENET file
+# Options to run it locally instead
+# MCR=/Applications/MATLAB/MATLAB_Runtime/v91 # Run on my Mac
+# MCR=/usr/local/MATLAB/MATLAB_Runtime/v92 # Run on linux (Selenium)
+MCR=/usr/local/matlab-runtime/r2017a/v92 # Run on ACENET
+
+
+
+cluster_name=fundy
+URL=titanium@$cluster_name.ace-net.ca
+dtnURL="$URL"
+if [ "$cluster_name" = "glooscap" ]; then
+	dtnURL=titanium@dtn.$cluster_name.ace-net.ca
+fi
+
+declare -i job_0=0
+declare -i job_f=0
+
+
+###############################################
+######### LOOP THROUGH JOB SCRIPTS ############
+###############################################
+# These loops happen within the cluster loops
+ssh -T -i ~/.ssh/id_rsa$cluster_name $URL << END
+	#mkdir $run_name
+	cd $run_name
+	#chmod +x $script_name run_$script_name.sh
+	###############################################
+	# Loop through job scripts
+	###############################################
+	for simnum in \`seq $job_0 $job_f\`; do
+		declare -i simnum_0=$simsize*\$simnum+1
+		declare -i simnum_f=$simsize+\$simnum_0-1
+		job_name=r$JobID\$simnum_0\_run_\$simnum_0\_to_\$simnum_f.job
+		###############################################
+		# The contents of the job script
+#######################################################
+cat > \$job_name << EOF
+#$ -cwd
+#$ -j yes
+#$ -l test=true
+#$ -l h_rt=0:10:0
+#$ -l h_vmem=1G
+( qstat | grep  $JobID\$simnum_0 | cut -d' ' -f1 )
+ls \$job_name.* | cut -d'.' -f3
+xkcd=$( ls \$job_name.* | cut -d'.' -f3 | cut -d'o' -f2)
+qstat -j $(ls \$job_name.* )
+echo INBETWEEN
+qstat -j $(ls \$job_name.* | cut -d'.' -f3 | cut -d'o' -f2)
+y=5
+z=$JobID\$simnum_0
+echo HI THERE
+echo \$xkcd \$y \$z
+echo HI AGAIN
+echo $xkcd $y $z
+echo BYE
+( qstat -j \$xkcd | grep maxvmem )
+( qstat -j \$( qstat | grep  $JobID\$simnum_0 | cut -d' ' -f1 ) | grep maxvmem )
+( ( qstat -j \$( qstat | grep  $JobID\$simnum_0 | cut -d' ' -f1 ) | grep maxvmem ) | cut -d'=' -f6 )
+echo hi hello
+./run_$script_name.sh $MCR $seed_0 \$simnum_0 \$simnum_f
+EOF
+#######################################################
+		# And finally Run ACENET Cluster
+		qsub \$job_name
+	# Finish job script loop
+	done
+# Save the Job-ID associated with this run (for maxvmem)
+#######################################################
+cat > qstat_$JobID.txt << EOF
+\$(qstat | grep $JobID)
+EOF
+# And just the list of jobs
+cat > joblist_$JobID.txt << EOF
+\$( (qstat | grep r05252) | cut -d' ' -f1 )
+EOF
+#######################################################
+END
+
+
+
+
+## Manual setup
+## Set up keygen on Selenium
+# ssh-keygen -t rsa #Hit enter three times
+# chmod go-w ~/
+# chmod 700 ~/.ssh
+# cd ~/.ssh && chmod 600 authorized_keys id_rsa id_rsa.pub known_hosts 
+# cat ~/.ssh/id_rsa.pub | ssh titanium@fundy.ace-net.ca "mkdir -p ~/.ssh && cat >>  ~/.ssh/authorized_keys && chmod go-w ~/ && chmod 700 ~/.ssh && cd ~/.ssh && chmod 600 authorized_keys"
+
+
+
+
+
+
+
+
+
+
