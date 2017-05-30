909a88e7e7168eae019414104295490b21ba693f
Stephanie Bland <Bland@Dal.ca>
2017-05-30 13:01:40 -0300

    2017May30_0

--- Shell_Scripts_for_Clusters/run_ACENET.sh
+++ Shell_Scripts_for_Clusters/run_ACENET.sh
@@ -168,7 +168,7 @@ for cluster_num in 0; do
 			END
 			# b) And uncompress them 
 			mv $JobID$cluster_name.zip ~/GIT/Analysis/$JobID$cluster_name.zip
-			unzip -q -j ~/GIT/Analysis/$JobID$cluster_name.zip -d  ~/GIT/Analysis/$run_name
+			unzip -q -j ~/GIT/Analysis/$JobID$cluster_name.zip -d ~/GIT/Analysis/$run_name
 			# c) When this is done, we can delete the crontab task
 			crontab -l > tmp_cron2.sh
 			sed -i '' "/$JobID$cluster_name/d" tmp_cron2.sh
