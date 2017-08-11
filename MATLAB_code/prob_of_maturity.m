6c10c84bff3371f58f7ea30b9d56b48250ed0b18
Stephanie Bland <Bland@Dal.ca>
2017-08-11 14:09:36 -0400

    Minor update

--- MATLAB_code/prob_of_maturity.m
+++ MATLAB_code/prob_of_maturity.m
@@ -8,7 +8,7 @@ reprod=zeros(nichewebsize,1);
 for j=find(is_split')
     stages=N_stages(j);
     %% Probability of Maturity (P)
-    a50 = starta50*(1- 0.005)^year;% a50 is age at which 50 reach maturity
+    a50 = starta50*(1- 0.005)^(year-1);% a50 is age at which 50% reach maturity
     sumL = 1 + exp(-3*((2:stages)-a50));
     P =[0, 1./sumL];
     mature_reprod=1-invest(1:stages);%percent invested in reproduction
